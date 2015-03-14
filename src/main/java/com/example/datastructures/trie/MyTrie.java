ce1ced37a5a5b1843291a9df8cc270cddfca8cbc
alvaro garcia <derecerca@gmail.com>
2015-03-14 22:35:49 +0100

    refactor: extract class Word to top level

--- src/main/java/com/example/datastructures/trie/MyTrie.java
+++ src/main/java/com/example/datastructures/trie/MyTrie.java
@@ -20,13 +20,13 @@ public class MyTrie {
 
 	public MyTrie add (final String value) {
 		Word word = Word.from(value);
-		if(!prefixes.contains(word.prefix)){
-			prefixes.add(word.prefix);
+		if(!prefixes.contains(word.prefix())){
+			prefixes.add(word.prefix());
 		}
-		if(!suffixes.containsKey(word.prefix)){
-			suffixes.put(word.prefix,new ArrayList<>());
+		if(!suffixes.containsKey(word.prefix())){
+			suffixes.put(word.prefix(),new ArrayList<>());
 		}
-		suffixes.get(word.prefix).add(word.suffix);
+		suffixes.get(word.prefix()).add(word.suffix());
 		return this;
 	}
 
@@ -44,16 +44,16 @@ public class MyTrie {
 	}
 
 	private boolean checkSuffixIfPresentFor (final Word word) {
-		if(!word.suffix.isPresent()){
+		if(!word.suffix().isPresent()){
 			return true;
 		}
 		levelCounter.oneMore();
-		return suffixes.get(word.prefix).contains(word.suffix);
+		return suffixes.get(word.prefix()).contains(word.suffix());
 	}
 
 	private boolean noMatchingPrefixFor (final Word word) {
 		levelCounter.oneMore();
-		if(!prefixes.contains(word.prefix)){
+		if(!prefixes.contains(word.prefix())){
 			return true;
 		}
 		return false;
@@ -63,37 +63,4 @@ public class MyTrie {
 		this.levelCounter = levelCounter;
 	}
 
-	private static class Word {
-		private final Character prefix;
-		private final Optional<String> suffix;
-
-		public Word (final char prefix, final String suffix) {
-			this(prefix, Optional.of(suffix));
-		}
-
-		public Word (final char prefix) {
-			this(prefix, Optional.empty());
-		}
-
-		public Word (final char prefix, Optional<String> suffix) {
-			this.prefix = prefix;
-			this.suffix = suffix;
-		}
-
-		@Override
-		public String toString () {
-			return "{" +
-					"prefix=" + prefix +
-					", suffix=" + suffix +
-					'}';
-		}
-
-		public static Word from (final String value) {
-			if(value.length() > 1){
-				return new Word(value.charAt(0), value.substring(1));
-			} else {
-				return new Word(value.charAt(0));
-			}
-		}
-	}
 }
---
+++ src/main/java/com/example/datastructures/trie/Word.java
@@ -0,0 +1,48 @@
+package com.example.datastructures.trie;
+
+import java.util.Optional;
+
+/**
+* Created by alvaro on 14/03/15.
+*/
+class Word {
+
+	private final Character prefix;
+	private final Optional<String> suffix;
+
+	public Word (final char prefix, final String suffix) {
+		this(prefix, Optional.of(suffix));
+	}
+
+	public Word (final char prefix) {
+		this(prefix, Optional.empty());
+	}
+
+	public Word (final char prefix, Optional<String> suffix) {
+		this.prefix = prefix;
+		this.suffix = suffix;
+	}
+
+	@Override
+	public String toString () {
+		return "{" +
+				"prefix=" + prefix +
+				", suffix=" + suffix +
+				'}';
+	}
+
+	public static Word from (final String value) {
+		if(value.length() > 1){
+			return new Word(value.charAt(0), value.substring(1));
+		} else {
+			return new Word(value.charAt(0));
+		}
+	}
+	public Character prefix () {
+		return prefix;
+	}
+
+	public Optional<String> suffix () {
+		return suffix;
+	}
+}
