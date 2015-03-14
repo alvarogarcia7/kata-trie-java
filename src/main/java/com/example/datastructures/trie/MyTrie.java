40a72cf0b9d9b1c20a7577c3eacbcbd06dab7edc
alvaro garcia <derecerca@gmail.com>
2015-03-14 20:22:25 +0100

    green

--- src/main/java/com/example/datastructures/trie/MyTrie.java
+++ src/main/java/com/example/datastructures/trie/MyTrie.java
@@ -1,10 +1,13 @@
 package com.example.datastructures.trie;
 
+import java.util.Optional;
+
 /**
  * Created by alvaro on 14/03/15.
  */
 public class MyTrie {
 	private boolean empty = true;
+	private Optional<String> value = Optional.empty();
 
 	public boolean isEmpty () {
 		return empty;
@@ -12,10 +15,14 @@ public class MyTrie {
 
 	public MyTrie add (final String value) {
 		empty = false;
+		this.value = Optional.of(value);
 		return this;
 	}
 
 	public boolean contains (final String value) {
+		if(this.value.isPresent()){
+			return this.value.get().equals(value);
+		}
 		return false;
 	}
 }
