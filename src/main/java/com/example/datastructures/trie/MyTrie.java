abfaff0fdd86acd3e00885f0768d7ff2c96395c7
alvaro garcia <derecerca@gmail.com>
2015-03-14 21:44:09 +0100

    refactor: convert prefixes into char

--- src/main/java/com/example/datastructures/trie/MyTrie.java
+++ src/main/java/com/example/datastructures/trie/MyTrie.java
@@ -10,7 +10,7 @@ import java.util.Optional;
  * Created by alvaro on 14/03/15.
  */
 public class MyTrie {
-	private List<String> prefixes = new ArrayList<>();
+	private List<Character> prefixes = new ArrayList<>();
 	private LevelCounter levelCounter = new LevelCounter();
 	private Map<Character, String> suffixes = new HashMap<>();
 
@@ -22,7 +22,7 @@ public class MyTrie {
 		if (hasSuffixAlreadyPresent(value)) {
 			this.suffixes.put(value.charAt(0), value.substring(0));
 		} else {
-			this.prefixes.add(value);
+			this.prefixes.add(value.charAt(0));
 		}
 		return this;
 	}
@@ -44,9 +44,6 @@ public class MyTrie {
 	}
 
 	public boolean contains (final String value) {
-		for (String prefix : prefixes) {
-			assert(prefix.length() == 1);
-		}
 
 		levelCounter.oneMore(); // check prefix
 		if (hasSuffixAlreadyPresent(value)) {
@@ -54,8 +51,8 @@ public class MyTrie {
 			final String suffix = suffixes.get(value.charAt(0));
 			return suffix.equals(value);
 		}
-		for (String current : prefixes) {
-			if (current.equals(value)) {
+		for (Character current : prefixes) {
+			if (current.equals(value.charAt(0))) {
 				return true;
 			}
 		}
@@ -67,7 +64,7 @@ public class MyTrie {
 	}
 
 	private class PrefixSuffix {
-		private final String prefix;
+		private final Character prefix;
 		private final Optional<String> suffix;
 
 		public PrefixSuffix (final char prefix, final String suffix) {
@@ -79,7 +76,7 @@ public class MyTrie {
 		}
 
 		public PrefixSuffix(final char prefix, Optional<String> suffix) {
-			this.prefix = String.valueOf(prefix);
+			this.prefix = prefix;
 			this.suffix = suffix;
 		}
 	}
