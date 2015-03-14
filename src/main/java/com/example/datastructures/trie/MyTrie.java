0be6898c2749f4ec0b9dde332884a9fdcc501090
alvaro garcia <derecerca@gmail.com>
2015-03-14 22:41:36 +0100

    refactor: move behaviour for Words.contains

--- src/main/java/com/example/datastructures/trie/MyTrie.java
+++ src/main/java/com/example/datastructures/trie/MyTrie.java
@@ -10,9 +10,9 @@ import java.util.Optional;
  * Created by alvaro on 14/03/15.
  */
 public class MyTrie {
-	private Words words = new Words();
 	private List<Character> prefixes = new ArrayList<>();
 	private LevelCounter levelCounter = new LevelCounter();
+	private Words words = new Words(levelCounter);
 	private Map<Character, List<Optional<String>>> suffixes = new HashMap<>();
 
 	public boolean isEmpty () {
@@ -44,30 +44,11 @@ public class MyTrie {
 
 	//TODO AGB this method has this name because I'm forecasting I'm going to extract a repository for Word
 	private boolean words_contain (final Word word) {
-		if (noMatchingPrefixFor(word)){
-			return false;
-		}
-		return checkSuffixIfPresentFor(word);
-	}
-
-	private boolean checkSuffixIfPresentFor (final Word word) {
-		if(!word.suffix().isPresent()){
-			return true;
-		}
-		levelCounter.oneMore();
-		return suffixes.get(word.prefix()).contains(word.suffix());
-	}
-
-	private boolean noMatchingPrefixFor (final Word word) {
-		levelCounter.oneMore();
-		if(!prefixes.contains(word.prefix())){
-			return true;
-		}
-		return false;
+		return words.contain(word);
 	}
 
 	public void injectLevelCounter (final LevelCounter levelCounter) {
-		this.levelCounter = levelCounter;
+		words.injectLevelCounter(levelCounter);
 	}
 
 }
--- src/main/java/com/example/datastructures/trie/Words.java
+++ src/main/java/com/example/datastructures/trie/Words.java
@@ -10,8 +10,14 @@ import java.util.Optional;
  * Created by alvaro on 14/03/15.
  */
 public class Words {
+	private LevelCounter levelCounter;
 	private List<Character> prefixes = new ArrayList<>();
 	private Map<Character, List<Optional<String>>> suffixes = new HashMap<>();
+
+	public Words (final LevelCounter levelCounter) {
+		this.levelCounter = levelCounter;
+	}
+
 	public void add (final Word word) {
 		if(!prefixes.contains(word.prefix())){
 			prefixes.add(word.prefix());
@@ -20,5 +26,32 @@ public class Words {
 			suffixes.put(word.prefix(),new ArrayList<>());
 		}
 		suffixes.get(word.prefix()).add(word.suffix());
+	}
+
+	public boolean contain (final Word word) {
+		if (noMatchingPrefixFor(word)){
+			return false;
+		}
+		return checkSuffixIfPresentFor(word);
+	}
+
+	private boolean checkSuffixIfPresentFor (final Word word) {
+		if(!word.suffix().isPresent()){
+			return true;
+		}
+		levelCounter.oneMore();
+		return suffixes.get(word.prefix()).contains(word.suffix());
+	}
+
+	private boolean noMatchingPrefixFor (final Word word) {
+		levelCounter.oneMore();
+		if(!prefixes.contains(word.prefix())){
+			return true;
+		}
+		return false;
+	}
+
+	public void injectLevelCounter (final LevelCounter levelCounter) {
+		this.levelCounter = levelCounter;
 	}
 }
