64da77ccc0d4878e1048a70d79ccc431a2c6f3b0
alvaro garcia <derecerca@gmail.com>
2015-03-14 20:09:08 +0100

    red: be_empty_by_default

---
+++ src/test/java/TriedShould.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * Created by alvaro on 14/03/15.
+ */
+public class TriedShould {
+
+	@Test
+	public void be_empty_by_default () {
+
+		org.hamcrest.MatcherAssert.assertThat(new MyTrie().isEmpty(), org.hamcrest.Matchers.is(true));
+	}
+}
